710c08b3b71af08f789311f8b5344ec03f9de9bc
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-01-05 05:02:30 +0200

    Variables and Data Types

---
+++ Basics/VariablesDataTypes.java
@@ -0,0 +1,20 @@
+package Basics;
+public class VariablesDataTypes{
+    public static void main (String[] args){
+        //Primitive Data Types (Not Change-able)
+        int helloWorld = 5;
+        double num2 = 5.0;
+        boolean b = false;
+        char c = 'C';
+        
+        //Non-Primitive
+        String str = "Carlos";
+
+        System.out.println(helloWorld);
+        System.out.println(num2);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(str);
+
+    }
+}
