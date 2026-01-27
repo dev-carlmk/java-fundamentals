747f79446fe70ce6067dc8f9bc1b83c90f02220f
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-01-27 22:44:17 +0200

    Parent Class (Object Comparison)

---
+++ OOP/ObjectComparison/Student.java
@@ -0,0 +1,29 @@
+package OOP.OverloadingMethod_ObjectComparison;
+public class Student implements Comparable<Student> {
+    private String name; // Instance Variable
+
+    // Constructor (Parent Class)
+    public Student (String name){
+        this.name = name;
+    }
+
+    // To Check for Equality between objects
+    public boolean equals(Student other){
+        if (this.name == other.name){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Comparison (Length between values)
+    public int compareTo(Student other){
+        return this.name.compareTo(other.name);
+    }
+
+    // Convert to String
+    public String toString(){
+        return this.name;
+    }
+
+}
