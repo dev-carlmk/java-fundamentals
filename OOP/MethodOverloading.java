42f3293f4ba26a88e1d3cd5e31bde3a24e65a0f6
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-01-31 21:40:28 +0200

    Method Overloading

---
+++ OOP/MethodOverloading.java
@@ -0,0 +1,17 @@
+package OOP;
+public class MethodOverloading {
+
+    // Method Overloading = same name, different parameters
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+}
