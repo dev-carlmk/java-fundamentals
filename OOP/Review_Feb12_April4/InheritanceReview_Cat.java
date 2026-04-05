35e3f268184c1c75d029e936a61471deed3cba94
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-04-05 19:24:57 +0200

    Inheritance Review - (Feb12 - April4)

---
+++ OOP/Review_Feb12_April4/InheritanceReview_Cat.java
@@ -0,0 +1,13 @@
+package OOP.Review_Feb12_April4;
+public class InheritanceReview_Cat extends ClassReview_Dog{
+    protected int food;
+
+    public InheritanceReview_Cat(String name, int age, int food){
+        super(name,age);
+        this.food = food;
+    }
+
+     public void speak(){
+        System.out.println("My name is " + this.name + " and I am " + this.age + " years old. I was fed " + this.food + " packs of milk today.");
+    }
+}
