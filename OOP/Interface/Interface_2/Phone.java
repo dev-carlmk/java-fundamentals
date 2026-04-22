4ed1ebde3e19445ceb7df0e173cd6c18508bfbfe
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-04-22 23:29:27 +0200

    Interface - Interface

---
+++ OOP/Interface/Interface_2/Phone.java
@@ -0,0 +1,10 @@
+package OOP.Interface.Interface_2;
+public interface Phone{
+
+    final double update = 1.5;
+
+    void changeVersion (double a);
+    void upgradePhone (int a);
+    void downgradePhone (int a);
+
+}
