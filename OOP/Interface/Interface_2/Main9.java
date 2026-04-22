3f7076cdeaa5ca6486a6f7faa4566d47199256a9
T. Carlos Kerwillain <tcarloskerwillain@gmail.com>
2026-04-22 23:28:18 +0200

    Main Class - Interface

---
+++ OOP/Interface/Interface_2/Main9.java
@@ -0,0 +1,10 @@
+package OOP.Interface.Interface_2;
+public class Main9{
+    public static void main (String[] args){
+        Device iPhone = new Device();
+        iPhone.changeVersion(2.0);
+        iPhone.upgradePhone(1);
+        iPhone.downgradePhone(2);
+        iPhone.display1();
+    }
+}
